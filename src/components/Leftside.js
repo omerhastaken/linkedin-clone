8e0fe0890be6eb06a13f13cb58a10668e508fb72
omerhastaken <omerefesermet@gmail.com>
2023-05-05 15:22:59 +0300

    modified:   src/components/Leftside.js

--- src/components/Leftside.js
+++ src/components/Leftside.js
@@ -27,9 +27,27 @@ const LeftSide = (props) => {
                 <Item>
                     <span>
                         <img src="images/item-icon.svg" />
+                        My Items
                     </span>
                 </Item>
             </Artcard>
+            <Sude>
+                <a>
+                    <span>Groups</span>
+                </a>
+                <a>
+                    <span>
+                        Events
+                        <img src="/images/plus-icon.svg" />
+                    </span>
+                </a>
+                <a>
+                    <span>Follow Hashtags</span>
+                </a>
+                <a>
+                    <span>Discover more</span>
+                </a>
+            </Sude>
         </Container>
     )
 }
@@ -105,36 +123,64 @@ const Widget = styled.div`
     padding-top: 12px;
     padding-bottom: 12px;
 
-    & < a {
+    & > a {
         text-decoration: none;
         display: flex;
         justify-content: space-between;
         align-items: center;
         padding: 4px 12px;
+        &:hover {
+          background-color: rgba(0, 0, 0, 0.08);
+        }
+        div {
+          display: flex;
+          flex-direction: column;
+          text-align: left;
+          span {
+            font-size: 12px;
+            line-height: 1.333;
+            &:first-child {
+              color: rgba(0, 0, 0, 0.6);
+            }
+            &:nth-child(2) {
+              color: rgba(0, 0, 0, 1);
+            }
+          }
+        }
+      }
+      svg {
+        color: rgba(0, 0, 0, 1);
+      }
+
+      svg {
+        color: rgba(0, 0, 0, 1);
+      }
+    `;
+
+const Item = styled.a`
+    border-color: rgba(0, 0, 0, 0.8); 
+    text-align: left;
+    padding: 12px;
+    font-size: 12px;
+    display: block;
+      
+    span {
+        display: flex;
+        align-items: center;
+        color: rgba(0, 0, 0, 1);
+        svg {
+            color: rgba(0, 0, 0, 0.6);
+        }
     }
 
     &:hover {
         background-color: rgba(0, 0, 0, 0.08);
 
-        div {
-            display: flex;
-            flex-direction: column;
-            text-align: left;
-            
-            span {
-                font-size: 12px;
-                line-height: 1.333;
-                &:firstchild{
-                    color: rgba(0, 0, 0, 0.6);
-                }
-                &:nth-child(2) {
-                    color: rgba(0, 0, 0, 1);
-                }
-            }
-        }
     }
 `
 
-const Item = styled.div``
+const Sude = styled.a`
+
+`
 
 export default LeftSide;
